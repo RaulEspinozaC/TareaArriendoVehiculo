d2b53ba54d8152ac2b3df88bf49d95dfb723fc47
RaulEspinozaC <48498822+RaulEspinozaC@users.noreply.github.com>
2019-09-27 22:11:27 -0300

    Update Arriendo.java

--- Arriendo.java
+++ Arriendo.java
@@ -2,12 +2,16 @@ public class Arriendo {
     private String fechaArriendo;
     private char medioPago;
     private int dias;
+    private Cliente pcli;
+    private Vehiculo pveh;
 
 
     public Arriendo(Vehiculo pveh, Cliente pcli, String pfec, char mp, int pdias){
         this.fechaArriendo= pfec;
         this.medioPago= mp;
         this.dias= pdias;
+        this.pcli=pcli;
+        this.pveh=pveh;
     }
 
     public String getFechaArriendo() {
@@ -22,36 +26,74 @@ public class Arriendo {
         return medioPago;
     }
 
-    public void setMedioPago(char medioPago) {
-        if (medioPago=='T' || medioPago=='E'){
-            this.medioPago = medioPago;
-        }
+    public void setMedioPago(char medioPago) { this.medioPago=medioPago; }
+
+    public int getDias() { return dias; }
+
+    public void setDias(int dias) { this.dias=dias; }
+
+    public Cliente getPcli() {
+        return pcli;
     }
 
-    public int getDias() {
-        return dias;
+    public void setPcli(Cliente pcli) {
+        this.pcli = pcli;
     }
 
-    public void setDias(int dias) {
-        if (dias < 11 && dias > 0){
-            this.dias = dias;
-        }
+    public Vehiculo getPveh() {
+        return pveh;
     }
 
-    public void imprimirDatosArriendo(){
-        System.out.println("Fecha Arriendo: "+getFechaArriendo());
-        System.out.println("Dias: "+getDias());
-        System.out.println("Medio de Pago: "+getMedioPago());
-
+    public void setPveh(Vehiculo pveh) {
+        this.pveh = pveh;
     }
 
-    //    I)
+    public void imprimirDatosArriendo() {
+        System.out.println("Fecha Arriendo: " + getFechaArriendo());
+        System.out.println("Dias: " + getDias());
+        System.out.println("Medio de Pago: " + getMedioPago());
+
+    }
 
     public boolean registrarVehiculoArrendado(){
-        if(Cliente.)
+        if(pcli.isVigencia() && !pveh.isSituacion()){
+            pveh.setSituacion(true);
+            return true;
+        }else{
+            return false;
+        }
     }
 
-    public
+    public int obtenerValorArriendo(int pprecio){
+        return getDias() * pprecio;
+    }
+
+    public void generarFichaArriendo(int pprecio){
+        String medio = null;
+        if(getMedioPago()=='T'){
+            medio="Tarjeta";
+        }else if (getMedioPago()=='E'){
+            medio="Efectivo";
+        }
+        System.out.println("                         F I C H A  A R R I E N D O  D E  V E H I C U L O");
+        System.out.println(                                                                                                        );
+        System.out.println("                              Vehiculo      : "+pveh.getPatente()+"  "+ pveh.getMarca() +"  " +pveh.getModelo());
+        System.out.println("                              Precio por dia: $"+ pprecio);
+        System.out.println("                                                                                                       ");
+        System.out.println("Fecha Arriendo   Rut Cliente   Nombre Cliente   Dias Arriendo   Valor Arriendo   Medio de Pago");
+        System.out.println("----------------------------------------------------------------------------------------------");
+        System.out.println("   "+getFechaArriendo()+"      "+pcli.getRut()+"      "+pcli.getNombre()+"              "+getDias()+"             $"+obtenerValorArriendo(pprecio)+"           "+medio);
+        System.out.println("----------------------------------------------------------------------------------------------");
+    }
+
+    public boolean registrarDevolucionVehiculo(String patente){
+        if(patente.equals(pveh.getPatente()) && pveh.isSituacion()){
+            pveh.setSituacion(false);
+            return true;
+        }else{
+            return false;
+        }
+    }
 
 
 }
