38433f13e78267de38c6dba32406dd49b74d7332
RaulEspinozaC <48498822+RaulEspinozaC@users.noreply.github.com>
2019-09-29 23:00:48 -0300

    Update PrincipalAdminEstudiantes.java

--- PrincipalAdminEstudiantes.java
+++ PrincipalAdminEstudiantes.java
@@ -3,6 +3,20 @@ import java.util.Scanner;
 public class PrincipalAdminEstudiantes {
     public static void main(String[] args){
 
+        String prut= null;
+        String pnom= null;
+        String ppatente= null;
+        String pmarca= null;
+        String pmod= null;
+        int paño= 0;
+        String pfec= null;
+        char mp= '\u0000';
+        int pdias=0;
+
+        Cliente pcli= new Cliente(prut,pnom);
+        Vehiculo pveh= new Vehiculo(ppatente,pmarca,pmod,paño);
+        Arriendo a1= new Arriendo(pveh,pcli,pfec,mp,pdias);
+
         show_menu();
 
         Scanner myObj= new Scanner(System.in);
@@ -10,74 +24,69 @@ public class PrincipalAdminEstudiantes {
         int opcion= myObj.nextInt();
 
         while(opcion != 4){
-
             switch (opcion){
 
                 case 1:
                     Scanner rut= new Scanner(System.in); // INGRESAR RUT
                     System.out.println("Ingrese el rut: ");
-                    String prut=rut.nextLine();
+                    a1.getPcli().setRut(rut.nextLine());
 
-                    if(!Validation.validationRut(prut)){     // VALIDAR RUT
+                    if(!Validation.validationRut(a1.getPcli().getRut())){     // VALIDAR RUT
                         System.out.println("Error: Rut Inválido.");
                         break;
                     }
 
                     Scanner nombre= new Scanner(System.in); // INGRESAR NOMBRE
                     System.out.println("Ingrese el nombre: ");
-                    String pnom=nombre.nextLine();
+                    a1.getPcli().setNombre(nombre.nextLine());
 
                     Scanner patente=new Scanner(System.in);   // INGRESAR PATENTE
                     System.out.println("Ingrese la patente: ");
-                    String ppatente=patente.nextLine();
+                    a1.getPveh().setPatente(patente.nextLine());
 
-                    if(!Validation.validationPatente(ppatente)){       // VALIDAR PATENTE
+                    if(!Validation.validationPatente(a1.getPveh().getPatente())){       // VALIDAR PATENTE
                         System.out.println("Error: Patente inválida.");
                         break;
                     }
 
                     Scanner marca= new Scanner(System.in);     // INGRESAR MARCA
                     System.out.println("Ingrese la marca: ");
-                    String pmarca=marca.nextLine();
+                    a1.getPveh().setMarca(marca.nextLine());
 
                     Scanner model=new Scanner(System.in);     // INGRESAR MODELO
                     System.out.println("Ingrese el modelo: ");
-                    String pmod=model.nextLine();
+                    a1.getPveh().setModelo(model.nextLine());
 
                     Scanner año=new Scanner(System.in);       // INGRESAR AÑO
                     System.out.println("Ingrese el año: ");
-                    int paño= año.nextInt();
+                    a1.getPveh().setAño(año.nextInt());
 
-                    if(!Validation.validationAño(paño)){       // VALIDAR AÑO
+                    if(!Validation.validationAño(a1.getPveh().getAño())){       // VALIDAR AÑO
                         System.out.println("Error: Año inválido.");
                         break;
                     }
 
                     Scanner fecha=new Scanner(System.in);      // INGRESAR FECHA
                     System.out.println("Ingrese la fecha de arriendo: ");
-                    String pfec=fecha.nextLine();
+                    a1.setFechaArriendo(fecha.nextLine());
 
                     Scanner medio=new Scanner(System.in);       // INGRESAR MEDIO DE PAGO
                     System.out.println("Ingrese el medio de pago: ");
-                    char mp= medio.next().charAt(0);
+                    a1.setMedioPago(medio.next().charAt(0));
 
-                    if(!Validation.validationMedioPago(mp)){          // VALIDAR MEDIO DE PAGO
+                    if(!Validation.validationMedioPago(a1.getMedioPago())){          // VALIDAR MEDIO DE PAGO
                         System.out.println("Error: Medio de pago inválido.");
                         break;
                     }
 
                     Scanner dias=new Scanner(System.in);             // INGRESAR DIAS DE ARRIENDO
                     System.out.println("Ingrese los días de arriendo");
-                    int pdias= dias.nextInt();
+                    a1.setDias(dias.nextInt());
 
-                    if(!Validation.validationDias(pdias)){          // VALIDAR LOS DIAS DE ARRIENDO
+                    if(!Validation.validationDias(a1.getDias())){          // VALIDAR LOS DIAS DE ARRIENDO
                         System.out.println("Error: No se puede arrendar por esa cantidad de días.");
                         break;
                     }
-
-                    Cliente pcli= new Cliente(prut,pnom);
-                    Vehiculo pveh= new Vehiculo(ppatente,pmarca,pmod,paño);
-                    Arriendo a1= new Arriendo(pveh,pcli,pfec,mp,pdias);
 
                     Scanner precio=new Scanner(System.in);
                     System.out.println("Ingrese el precio diario: ");
@@ -99,13 +108,17 @@ public class PrincipalAdminEstudiantes {
                     String patenteVeh= myObj2.nextLine();
 
                     a1.registrarDevolucionVehiculo(patenteVeh);
+
+                    System.out.println("Devolución Exitosa.");
                     break;
 
                 case 3:
                     a1.getPcli().setVigencia(true);
                     break;
             }
-            
+
+            show_menu();
+
             Scanner myObj3= new Scanner(System.in);
             System.out.println("Ingrese su opción: "); // INGRESAR LA OPCION
             opcion= myObj3.nextInt();
